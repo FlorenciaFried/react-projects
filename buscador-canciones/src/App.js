75bec8efa3972d37d2793e20a0a0e79f9f09ef1e
FlorenciaFried <florencia.d.fried@gmail.com>
2020-04-20 15:55:23 -0300

    Proyecto termiado: consulta a la API para obtener la info del artista y componente Info para mostrarla

--- buscador-canciones/src/App.js
+++ buscador-canciones/src/App.js
@@ -1,12 +1,14 @@
 import React, { Fragment, useState, useEffect } from "react";
 import Form from "./components/Form";
 import Song from "./components/Song";
+import Info from './components/Info';
 import axios from "axios";
 
 function App() {
   // Definir busqueda
   const [songSearch, setSongSearch] = useState({});
-  const [lyrics, setLyrics] = useState("");
+  const [lyrics, setLyrics] = useState('');
+  const [artist, setArtist] = useState('');
 
   useEffect(() => {
     if (Object.keys(songSearch).length === 0) return;
@@ -15,19 +17,19 @@ function App() {
       const { artist, song } = songSearch;
 
       const url = `https://api.lyrics.ovh/v1/${artist}/${song}`;
-      //const url2 = `https://www.theaudiodb.com/api/v1/json/1/search.php?s=${artista}`;
+      const url2 = `https://www.theaudiodb.com/api/v1/json/1/search.php?s=${artist}`;
 
-      const result = await axios(url);
-      setLyrics(result.data.lyrics);
-
-      /*const [letra, informacion] = await Promise.all([
+      const [lyrics, info] = await Promise.all([
         axios(url),
         axios(url2)
-      ]);*/
+      ]);
+
+      setLyrics(lyrics.data.lyrics);
+      setArtist(info.data.artists[0]);
     };
 
     consultApiLyrics();
-  }, [songSearch]);
+  }, [songSearch, artist]);
 
   return (
     <Fragment>
@@ -35,7 +37,11 @@ function App() {
 
       <div className="container mt-5">
         <div className="row">
-          <div className="col-md-6"></div>
+          <div className="col-md-6">
+          <Info 
+            artist={artist}
+          />
+          </div>
           <div className="col-md-6">
             <Song lyrics={lyrics} />
           </div>
